4585ec6739a7cdbc5673ae2493045006f77bc5f4
gxaurab <oe.gaurab@gmail.com>
2023-04-29 21:06:12 +0545

    minor changes

--- login pages/connect.php
+++ login pages/connect.php
@@ -1,7 +1,21 @@
-<?php 
-$Full Name = $_POST['Full Name'];
-$Email = $_POST['Email'];
-$Password = $_POST['Password'];
-$Mobile = $_POST['Mobile'];
-    //Database connection
+<?php
+	$full_name = $_POST['full_name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$mobile = $_POST['mobile'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','Customers');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(full_name ,email, password, mobile) values(?, ?, ?, ?)");
+		$stmt->bind_param("sssi", $full_name,$email, $password, $mobile);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
 ?>
